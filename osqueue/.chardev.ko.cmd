7518c376831414636f058f2e2c78724f9af799ae
root <root@localhost.localdomain>
2013-10-24 17:36:00 -0400

    added scripts for convenience

---
+++ osqueue/.chardev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/Desktop/OS_hw/osqueue/chardev.ko := ld -m elf_i386 -m elf_i386 -r -o /root/Desktop/OS_hw/osqueue/chardev.ko /root/Desktop/OS_hw/osqueue/chardev.o /root/Desktop/OS_hw/osqueue/chardev.mod.o
---
+++ osqueue/.tmp_versions/chardev.mod
@@ -0,0 +1,2 @@
+/root/Desktop/OS_hw/osqueue/chardev.ko
+/root/Desktop/OS_hw/osqueue/chardev.o
